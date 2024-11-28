574457d274e67932b611a859611815fc40c5b976
unknown <jaredtiu1706@gmail.com>
2024-11-28 12:49:48 +0800

    made a working input loop that displays in the txt file

--- user_input_personal_information_txt_file.py
+++ user_input_personal_information_txt_file.py
@@ -4,15 +4,16 @@ file_txt = open("./output.txt", "w")
 #make a loop asking if the user wants to continue 
 #write the stored value in the array in the txt file
 
-name_list = []
+name_array = []
 while True: 
 
     name = input("Input name: ")
-    name_list.append(name)
+    name_array.append(name)
 
     retry = input("Do you want to contiue? y/n: ")
 
     if retry != "y": 
         break
 
-file_txt.write(name_list)
+for i in name_array:
+    file_txt.write(f"{i} \n" )
