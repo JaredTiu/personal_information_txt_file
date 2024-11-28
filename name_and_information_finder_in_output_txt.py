f26b4af7210e73b83076d6e7c395b10b9d0cda47
unknown <jaredtiu1706@gmail.com>
2024-11-28 23:36:29 +0800

    trying to make a function to find the names in lines

--- name_and_information_finder_in_output_txt.py
+++ name_and_information_finder_in_output_txt.py
@@ -4,6 +4,7 @@
 
 output_file = "output.txt"
 
+information = []
 try: 
     #read the output file 
     with open(output_file, "r") as file:
@@ -11,5 +12,14 @@ try:
         lines = file.readlines
     #ask user for name input
         name_search = input("Who are you searching for?: ")
+
+    for i in lines: 
+        if lines.strip().startswith("Name: ") and name_search in lines: 
+            exists = True
+            information = lines[i:i+6] #since every person has six lines between them
+            break
+
 except FileNotFoundError:
     print("File not found")
+
+print(information)
