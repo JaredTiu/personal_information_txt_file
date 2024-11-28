e71eef98f3bc968085462ed344c2b9bc93bce698
unknown <jaredtiu1706@gmail.com>
2024-11-28 21:22:38 +0800

    added a valid name checker

--- user_input_personal_information_txt_file.py
+++ user_input_personal_information_txt_file.py
@@ -1,9 +1,12 @@
 import datetime
 
-file_txt = open("./output.txt", "w")
+def valid_name(name):
+    for x in name: 
+        if not x.isalpha() or x == "":
+            return False
+    return True 
 
-#make a loop asking if the user wants to continue 
-#write the stored value in the array in the txt file
+file_txt = open("./output.txt", "w")
 
 name_array = []
 age_array = []
@@ -14,12 +17,16 @@ converted_array = []
 #get the personal info and store in a array 
 while True: 
     name = input("Input name: ")
-
+    while not valid_name(name) or name.isspace() or name.strip() == "":
+       name = input("please enter a valid name: ")
     name_array.append(name)
 
     while True:    
         try: 
             age = int(input("Input age: "))
+
+            if age <=0 or age >=120:
+                raise
             age_array.append(age)
             break
         except:
@@ -40,13 +47,15 @@ while True:
         else: 
             print("Invalid input")
             input("Input again, binibilang mo ba ang butil ng magic sarap? y/n: ")
-
+    
+    #make a loop asking if the user wants to continue 
     retry = input("Do you want to contiue? y/n: ")
 
     if retry != "y": 
         print("Information has been printed in the txt file, Thank you.")
         break
 
+#write the stored value in the array in the txt file
 for i in range(len(name_array)):
     file_txt.write(f"Name: {name_array[i]} \n" )
     file_txt.write(f"Age: {age_array[i]} \n" )
