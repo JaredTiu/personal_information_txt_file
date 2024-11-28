9f529127a782bf59e4a0057d4ac3a770eae7fa00
unknown <jaredtiu1706@gmail.com>
2024-11-28 12:16:07 +0800

    testing how to add inputs to txt file

--- user_input_personal_information_txt_file.py
+++ user_input_personal_information_txt_file.py
@@ -3,3 +3,16 @@ file_txt = open("./output.txt", "w")
 #get the personal info and store in a array 
 #make a loop asking if the user wants to continue 
 #write the stored value in the array in the txt file
+
+name_list = []
+while True: 
+
+    name = input("Input name: ")
+    name_list.append(name)
+
+    retry = input("Do you want to contiue? y/n: ")
+
+    if retry != "y": 
+        break
+
+file_txt.write(name_list)
