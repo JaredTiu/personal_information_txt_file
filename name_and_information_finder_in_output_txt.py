3c24cf582267216c369c9266f3f28da3d0466e6b
unknown <jaredtiu1706@gmail.com>
2024-11-29 18:22:42 +0800

    still not working changed some things in for loop

--- name_and_information_finder_in_output_txt.py
+++ name_and_information_finder_in_output_txt.py
@@ -8,17 +8,16 @@ try:
     #read the output file 
     with open(output_file, "r") as file:
         #this reads each lines in the file
-        read = file.readlines
+        read = file.readlines()
     #ask user for name input
         name_search = input("Who are you searching for?: ")
         exists = False
         information = []
 
-        for i in file: 
-            # if i.startswith(name_search) in file: 
-            if name_search in i: 
+        for i, line in enumerate(read): 
+            if line.startswith("Name: ") and name_search in read: 
                 exists = True
-                information = file[i:i+6] #since every person has six lines between them
+                info = information.append(line[i:i+6]) #since every person has six lines between them
                 break
 
 except FileNotFoundError:
