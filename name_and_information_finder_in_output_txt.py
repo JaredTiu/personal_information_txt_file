789c3c58708d66538af91105bbb19d75d4401cfb
unknown <jaredtiu1706@gmail.com>
2024-11-29 15:32:53 +0800

    trying to make it work but it says the info array is not iretable

--- name_and_information_finder_in_output_txt.py
+++ name_and_information_finder_in_output_txt.py
@@ -4,18 +4,19 @@
 
 output_file = "output.txt"
 
-information = []
 try: 
     #read the output file 
     with open(output_file, "r") as file:
         #this reads each lines in the file
-        
+        read = file.readlines
     #ask user for name input
         name_search = input("Who are you searching for?: ")
         exists = False
+        information = []
 
         for i in file: 
-            if i.startswith("Name:") and name_search in file: 
+            # if i.startswith(name_search) in file: 
+            if name_search in i: 
                 exists = True
                 information = file[i:i+6] #since every person has six lines between them
                 break
