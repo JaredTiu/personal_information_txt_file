f6f151322f1ce7553360e654f2f54715e59c36b9
unknown <jaredtiu1706@gmail.com>
2024-11-29 20:58:02 +0800

    set the definitions for valid names

--- name_and_information_finder_in_output_txt.py
+++ name_and_information_finder_in_output_txt.py
@@ -15,6 +15,8 @@ try:
             #ask user for name input
         while True:
             name_search = input("Who are you searching for?(enter the full name): ").lower()
+            while not valid_name(name_search) or name_search.isspace() or name_search.strip() == "":
+                name_search = input("please enter a valid name: ")
             exists = False
         
             for i, line in enumerate(read): 
